0b7244707e24f102f370a3917c9a00bad66d00b7
Timofey Solonin <tim-solonin2@yandex.ru>
2017-09-16 11:28:47 +0300

    Added pods

---
+++ BackInvest/Stories/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BackInvest
+//
+//  Created by Timofey on 9/16/17.
+//  Copyright © 2017 Jufy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Pods/Target Support Files/Pods-BackInvest/Pods-BackInvest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BackInvest : NSObject
+@end
+@implementation PodsDummy_Pods_BackInvest
+@end
