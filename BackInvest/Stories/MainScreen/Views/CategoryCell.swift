525fa941c6debad2b509c4eaefe60f7cfb32f53e
Artemiy <temitrix@gmail.com>
2017-09-16 22:15:55 +0300

    completed category cell

--- BackInvest/Stories/MainScreen/Views/CategoryCell.swift
+++ BackInvest/Stories/MainScreen/Views/CategoryCell.swift
@@ -5,7 +5,22 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 class CategoryCell: UITableViewCell {
 
+    var categoryView: InvestmentCategoryCellView = InvestmentCategoryCellView()
+
+    init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(categoryView)
+        categoryView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    func configured(with category: InvestmentCategoryInCell) -> Self {
+        categoryView.setup(with: category)
+        return self
+    }
 }
